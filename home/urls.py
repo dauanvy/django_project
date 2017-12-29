37bf2edc2161dd6e74e165d425eea3e618449ae4
dautienthuy <dautienthuy@gmail.com>
2017-12-28 23:23:23 -0800

    [ADD]Them bang category, them phan tim kiem trong template trang chu

--- home/urls.py
+++ home/urls.py
@@ -9,7 +9,12 @@ urlpatterns = [
 	url(r'^event_bk_update/(?P<id>[\d]+)$',home_views.event_bk_update,name='event_bk_update'),
 	url(r'^event_bk_remove/(?P<id>[\d]+)$',home_views.event_bk_remove,name='event_bk_remove'),
 	url(r'^(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(model=Events,),name='events_detail'),
+	
 	url(r'^location_bk$',home_views.location_bk,name='location_bk'),
 	url(r'^location_bk_update/(?P<id>[\d]+)$',home_views.location_bk_update,name='location_bk_update'),
 	url(r'^location_bk_remove/(?P<id>[\d]+)$',home_views.location_bk_remove,name='location_bk_remove'),
+	
+	url(r'^category_bk$',home_views.category_bk,name='category_bk'),
+	url(r'^category_bk_update/(?P<id>[\d]+)$',home_views.category_bk_update,name='category_bk_update'),
+	url(r'^category_bk_remove/(?P<id>[\d]+)$',home_views.category_bk_remove,name='category_bk_remove'),
 ]
